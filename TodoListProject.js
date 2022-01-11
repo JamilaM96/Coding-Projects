edceaf80e7511644ad7f82d3c62fa0806efa3a05
JamilaM96 <96748962+JamilaM96@users.noreply.github.com>
2022-01-10 19:40:34 -0500

    TodoListProject.js

---
+++ TodoListProject.js
@@ -0,0 +1,24 @@
+let input = prompt ("what would you like to do");
+const todos = ['collect eggs', 'clean cats'];
+while (input !== "quit" && input !== "q") {
+	if (input === "list") {
+		console.log("")
+		for(let i = 0; i < todos.length; i++){
+			console.log('${1}: ${todos[i]');
+		}
+		console.log("")
+	} else if(input == "new"){
+		const newTodo = prompt('Ok, what is the new todo?');
+		todos.push(newTodo);
+		console.log('${newTodo} added to the list')
+	}
+	else if (input === 'delete'){
+		const index = prompt('Ok, enter an index to delete')
+		const deleted = todos.splice(index,1);
+		console.log('Ok,deleted ${deleted[0]}');
+	}
+	input = prompt("what would you like to do?")
+		
+	
+}
+console.log("OK, QUIT THE APP!")
